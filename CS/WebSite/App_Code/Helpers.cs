541a742f895b0db25f4029424d6a6a7f430fe465
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/Helpers.cs
@@ -0,0 +1,89 @@
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.XtraScheduler;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public static class DataHelper
+{
+    public static void SetupMappings(ASPxScheduler control)
+    {
+        ASPxSchedulerStorage storage = control.Storage;
+        ASPxAppointmentMappingInfo mappings = storage.Appointments.Mappings;
+        storage.BeginUpdate();
+        try {
+            mappings.AppointmentId = "Id";
+            mappings.Start = "StartTime";
+            mappings.End = "EndTime";
+            mappings.Subject = "Subject";
+            mappings.AllDay = "AllDay";
+            mappings.Description = "Description";
+            mappings.Label = "Label";
+            mappings.Location = "Location";
+            mappings.RecurrenceInfo = "RecurrenceInfo";
+            mappings.ReminderInfo = "ReminderInfo";
+            mappings.ResourceId = "OwnerId";
+            mappings.Status = "Status";
+            mappings.Type = "EventType";
+            #region #custommappings
+            storage.Appointments.CustomFieldMappings.Add
+                (new ASPxAppointmentCustomFieldMapping("Field1", "Amount"));
+            storage.Appointments.CustomFieldMappings.Add
+                (new ASPxAppointmentCustomFieldMapping("Field2", "Memo"));
+            #endregion #custommappings
+        }
+        finally {
+            storage.EndUpdate();
+        }
+    }
+
+    public static void ProvideRowInsertion(ASPxScheduler control, DataSourceControl dataSource)
+    {
+        ObjectDataSource objectDataSource = dataSource as ObjectDataSource;
+        if (objectDataSource != null) {
+            ObjectDataSourceRowInsertionProvider provider = new ObjectDataSourceRowInsertionProvider();
+            provider.ProvideRowInsertion(control, objectDataSource);
+        }
+    }
+}
+public class ObjectDataSourceRowInsertionProvider
+{
+    object lastInsertedId;
+
+    public void ProvideRowInsertion(ASPxScheduler control, ObjectDataSource dataSource)
+    {
+        control.AppointmentsInserted += new PersistentObjectsEventHandler(control_AppointmentsInserted);
+        dataSource.Inserted += new ObjectDataSourceStatusEventHandler(dataSource_Inserted);
+    }
+    void dataSource_Inserted(object sender, ObjectDataSourceStatusEventArgs e)
+    {
+        this.lastInsertedId = e.ReturnValue;
+    }
+    void control_AppointmentsInserted(object sender, PersistentObjectsEventArgs e)
+    {
+        ASPxSchedulerStorage storage = (ASPxSchedulerStorage)sender;
+        System.Diagnostics.Debug.Assert(e.Objects.Count == 1);
+        Appointment apt = (Appointment)e.Objects[0];
+        storage.SetAppointmentId(apt, this.lastInsertedId);
+    }
+}
+
+public class ResourceFiller
+{
+    public static string[] Users = new string[] { "Sarah Brighton", "Ryan Fischer", "Andrew Miller" };
+    public static string[] Usernames = new string[] { "sbrighton", "rfischer", "amiller" };
+
+    public static void FillResources(ASPxSchedulerStorage storage, int count)
+    {
+        ResourceCollection resources = storage.Resources.Items;
+        storage.BeginUpdate();
+        try {
+            int cnt = System.Math.Min(count, Users.Length);
+            for (int i = 1; i <= cnt; i++) {
+                resources.Add(storage.CreateResource(Usernames[i - 1], Users[i - 1]));
+            }
+        }
+        finally {
+            storage.EndUpdate();
+        }
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,81 @@
+﻿Imports System
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.XtraScheduler
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Private ReadOnly Property Storage() As ASPxSchedulerStorage
+        Get
+            Return Me.ASPxScheduler1.Storage
+        End Get
+    End Property
+    Public Shared RandomInstance As New Random()
+
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        DataHelper.SetupMappings(Me.ASPxScheduler1)
+        ResourceFiller.FillResources(Me.ASPxScheduler1.Storage, 3)
+        DataHelper.ProvideRowInsertion(ASPxScheduler1, appointmentDataSource)
+
+        ASPxScheduler1.AppointmentDataSource = appointmentDataSource
+        ASPxScheduler1.DataBind()
+    End Sub
+
+    Private Function GetCustomEvents() As CustomEventList
+
+        Dim events_Renamed As CustomEventList = TryCast(Session("ListBoundModeObjects"), CustomEventList)
+        If events_Renamed Is Nothing Then
+            events_Renamed = GenerateCustomEventList()
+            Session("ListBoundModeObjects") = events_Renamed
+        End If
+        Return events_Renamed
+    End Function
+
+    #Region "Random events generation"
+    Private Function GenerateCustomEventList() As CustomEventList
+        Dim eventList As New CustomEventList()
+        Dim count As Integer = Storage.Resources.Count
+        For i As Integer = 0 To count - 1
+            Dim resource As Resource = Storage.Resources(i)
+            Dim subjPrefix As String = resource.Caption & "'s "
+
+            eventList.Add(CreateEvent(resource.Id, subjPrefix & "meeting", 2, 5))
+            eventList.Add(CreateEvent(resource.Id, subjPrefix & "travel", 3, 6))
+            eventList.Add(CreateEvent(resource.Id, subjPrefix & "phone call", 0, 10))
+        Next i
+        Return eventList
+    End Function
+    Private Function CreateEvent(ByVal resourceId As Object, ByVal subject As String, ByVal status As Integer, ByVal label As Integer) As CustomEvent
+        Dim customEvent As New CustomEvent()
+        customEvent.Subject = subject
+        customEvent.OwnerId = resourceId
+        Dim rnd As Random = RandomInstance
+        Dim rangeInHours As Integer = 48
+        customEvent.StartTime = Date.Today + TimeSpan.FromHours(rnd.Next(0, rangeInHours))
+        customEvent.EndTime = customEvent.StartTime.Add(TimeSpan.FromHours(rnd.Next(0, rangeInHours \ 8)))
+        customEvent.Status = status
+        customEvent.Label = label
+        customEvent.Id = "ev" & customEvent.GetHashCode()
+        Return customEvent
+    End Function
+    #End Region
+        #Region "#appointmentformshowing"
+    Protected Sub ASPxScheduler1_AppointmentFormShowing(ByVal sender As Object, ByVal e As AppointmentFormEventArgs)
+        e.Container = New UserAppointmentFormTemplateContainer(DirectCast(sender, ASPxScheduler))
+    End Sub
+    #End Region ' #appointmentformshowing
+
+    Protected Sub appointmentsDataSource_ObjectCreated(ByVal sender As Object, ByVal e As ObjectDataSourceEventArgs)
+        e.ObjectInstance = New CustomEventDataSource(GetCustomEvents())
+    End Sub
+    #Region "#beforeexecutecallbackcommand"
+    Protected Sub ASPxScheduler1_BeforeExecuteCallbackCommand(ByVal sender As Object, ByVal e As SchedulerCallbackCommandEventArgs)
+        If e.CommandId = SchedulerCallbackCommandId.AppointmentSave Then
+            e.Command = New UserAppointmentSaveCallbackCommand(DirectCast(sender, ASPxScheduler))
+        End If
+    End Sub
+    #End Region ' #beforeexecutecallbackcommand
+End Class
